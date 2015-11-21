308ca5037eb8ef6b6aa2d79690312f505df24a02
Steven Marturano <steven.marturano@gmail.com>
2015-11-21 01:36:20 -0500

    menu added

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
-def prompt():
-    print "prompt"
-
-prompt()
-
 def encode(regular_message):
     return
+
 
 def decode(encrypted_message):
     return
 
+
 def print_message(op, message):
-    print
+    #op true = encode, op false = fasle
+    if op == True:
+        print encode(message)
+    else:
+        print decode (message)
+
+
+def prompt():
+    operation = raw_input("Do you want to encode or decode a message?\na) encode\nb) decode\n")
+    if operation.lower() == "a":
+        op = True
+        message = raw_input("Enter message to encode: ")
+    elif operation.lower() == "b":
+        op = False
+        message = raw_input("Enter message to decode: ")
+    else:
+        print "Input unrecognized, try again!"
+        prompt()
+    print_message(op, message)
+
+prompt()
+
+
+
+
+
 
