7e4b7b3ba948785caecf127c031f6cba55933dcf
Stefanie Epp <stefanie.epp@stonybrook.edu>
2015-11-21 01:09:46 -0500

    encode makes things lowercase and gets rid of special characters

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+import re
+
+
 def prompt():
     print "prompt"
 
 prompt()
 
+
 def encode(regular_message):
-    return
+    regular_message = re.sub("[\W\d_]+", "", regular_message.strip()).lower()
+    return regular_message
+
 
 def decode(encrypted_message):
     return
 
+
 def print_message(op, message):
     print
 
+#print encode("sWWWWj423!!@@@_[]()&^%$#@JMNHBGVFHRTNYJTMYRT~!@#$^*)8&^%$&<><>,,.,.765434kl~>>>*&grffeytr")
